6fcac929e945df4f4729fb382eb929b2f47c23a6
kenichi.shiratsuchi <gdk.shiratsuchi@gmail.com>
2021-04-20 04:37:56 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 白土顕一 on 2021/04/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
